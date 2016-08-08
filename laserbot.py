d39874739f9bcad08180eed23ab80c46a8a850fc
Andrew Elwell <andrew.elwell@gmail.com>
2016-08-08 23:46:32 +0800

    don't fail on unknown card prefix

--- laserbot.py
+++ laserbot.py
@@ -51,7 +51,7 @@ GPIO.output(20, False)
 import serial
 serial = serial.Serial("/dev/ttyAMA0", baudrate=9600)
 
-types = {'6F': 'Card','28':'Fob'}
+types = {'6F': 'Card','28':'Fob','1D':'Card', '1C':'Card'}
 knowncards = {}
 
 # list of valid cards
@@ -90,7 +90,11 @@ while True:
         card = code[-12:-2]
         prefix = card[0:2]
         cardno = card[2:10]
-        cardstr = card,types[prefix],int(cardno,16)
+        if prefix in types:
+            tagtype = types[prefix]
+        else:
+            tagtype = 'RFID'
+        cardstr = card,tagtype,int(cardno,16)
         print cardstr
         code = ''
         if knowncards.has_key(card):
