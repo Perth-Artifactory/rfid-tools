7507104a54ae57e19233bbc549c59fd3d96fcf08
Andrew Elwell <andrew.elwell@gmail.com>
2016-10-06 00:43:01 +0800

    Added explicit import sys
    Debugging why it failed if unable to irk with sys errror. Turns out
    calling sys.stderr.write isn't possible if called from an Except
    loop.

--- doorbot.py
+++ doorbot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 from time import gmtime, strftime
 import time
 import RPi.GPIO as GPIO
@@ -13,8 +14,8 @@ cardfile = '/home/pi/known_cards.csv'
 
 
 # merged in irker client to notify the IRC channel when door opened
-irker_server = ("core", 6659)
-target = "ircs://chat.freenode.net/artifactory"
+irker_server = ('core', 6659)
+target = 'ircs://chat.freenode.net/artifactory'
 subsystem = 'Doorbot: '
 curtime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 def connect():
@@ -59,51 +60,51 @@ irk('Started - %d cards loaded' % len(knowncards))
 code = ''
 
 def unlock_door():
-        GPIO.output(21, True)
-        time.sleep(3)
-        GPIO.output(21, False)
+	GPIO.output(21, True)
+	time.sleep(3)
+	GPIO.output(21, False)
 
 while True:
-        data = serial.read()
-        input_state = GPIO.input(13)
-        if data == '\r':
-                card = code[-12:-2]
-                prefix = card[0:2]
-                cardno = card[2:10]
-                if prefix in types:
-                        tagtype = types[prefix]
-                else:
-                        tagtype = 'RFID'
-                cardstr = card,tagtype,int(cardno,16)
-                print cardstr
-                code = ''
-                if knowncards.has_key(card):
-                        print "KNOWN %s %s presented at door" % (cardstr[1],cardstr[2])
-                        # Has card been disabled?
-                        if knowncards[card][1]:
-                                print "DISABLED: " + knowncards[card][0]
-                                os.system ("mpg123 -q /opt/sounds/denied.mp3 &")
-                                syslog.syslog('DENIED: %s (Card Disabled) %s' % (knowncards[card][0],int(cardno,16)))
-                                irk('\x1b[31m'+knowncards[card][0] +'\x1b[0m denied access')
-                        else:
-                                if (knowncards[card][0] == ''):
-                                        print "ALLOWED UNMAPPED USER"
-                                        syslog.syslog('ALLOWED: UNMAPPED USER %s' % int(cardno,16))
-                                        irk('\x1b[33mUnknown User\x1b[0m opened the door')
-                                else:
-                                        print "ALLOWED: " + knowncards[card][0]
-                                        syslog.syslog('ALLOWED: %s %s' % (knowncards[card][0],int(cardno,16)))
-                                        irk('\x1b[32m' + knowncards[card][0]+ 'opened the door')
-                                        #cmd="curl -g http://10.60.210.69/entryapi.php?entryname='%s'?time='%s'" % (knowncards[card][0],curtime)
-                                        #os.system(cmd)
-                                os.system ("mpg123 -q /opt/sounds/granted.mp3 &")
-                                unlock_door()
-                else:
-                        print "UNKNOWN Card/Fob"
-                        syslog.syslog('DENIED ' + str(cardstr))
-                        os.system ("mpg123 -q /opt/sounds/denied.mp3 &")
-                        irk('\x1b[31mUnknown Card\x1b[0m presented at door')
-        else:
-                code = code + data
+	data = serial.read()
+	input_state = GPIO.input(13)
+	if data == '\r':
+		card = code[-12:-2]
+		prefix = card[0:2]
+		cardno = card[2:10]
+		if prefix in types:
+			tagtype = types[prefix]
+		else:
+			tagtype = 'RFID'
+		cardstr = card,tagtype,int(cardno,16)
+		print cardstr
+		code = ''
+		if knowncards.has_key(card):
+			print "KNOWN %s %s presented at door" % (cardstr[1],cardstr[2])
+			# Has card been disabled?
+			if knowncards[card][1]:
+				print "DISABLED: " + knowncards[card][0]
+				os.system ("mpg123 -q /opt/sounds/denied.mp3 &")
+				syslog.syslog('DENIED: %s (Card Disabled) %s' % (knowncards[card][0],int(cardno,16)))
+				irk('\x1b[31m'+knowncards[card][0] +'\x1b[0m denied access')
+			else:
+				if (knowncards[card][0] == ''):
+					print "ALLOWED UNMAPPED USER"
+					syslog.syslog('ALLOWED: UNMAPPED USER %s' % int(cardno,16))
+					irk('\x1b[33mUnknown User\x1b[0m opened the door')
+				else:
+					print "ALLOWED: " + knowncards[card][0]
+					syslog.syslog('ALLOWED: %s %s' % (knowncards[card][0],int(cardno,16)))
+					irk('\x1b[32m' + knowncards[card][0]+ '\x1b[0m opened the door')
+					#cmd="curl -g http://10.60.210.69/entryapi.php?entryname='%s'?time='%s'" % (knowncards[card][0],curtime)
+					#os.system(cmd)
+				os.system ("mpg123 -q /opt/sounds/granted.mp3 &")
+				unlock_door()
+		else:
+			print "UNKNOWN Card/Fob"
+			syslog.syslog('DENIED ' + str(cardstr))
+			os.system ("mpg123 -q /opt/sounds/denied.mp3 &")
+			irk('\x1b[31mUnknown Card\x1b[0m presented at door')
+	else:
+		code = code + data
 
 # unlikely to get here...
